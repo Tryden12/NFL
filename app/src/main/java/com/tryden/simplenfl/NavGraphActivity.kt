287e90668f50c11addd337f6304ef2a3b482e8e0
Tyler Ryden <tryden11@gmail.com>
2023-01-25 13:02:36 -0500

    bottom nav in nav graph activity layout setup successfully layout wise

--- app/src/main/java/com/tryden/simplenfl/NavGraphActivity.kt
+++ app/src/main/java/com/tryden/simplenfl/NavGraphActivity.kt
@@ -3,16 +3,41 @@ package com.tryden.simplenfl
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NavGraphActivity: AppCompatActivity() {
 
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav_graph)
+        setupBottomNav()
+
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+    }
+
+    private fun setupBottomNav() {
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homePage -> {
+                    true
+                }
+                R.id.teamsListPage -> {
+                    true
+                }
+                R.id.scoresPage -> {
+                    true
+                }
+                R.id.newsPage -> {
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
