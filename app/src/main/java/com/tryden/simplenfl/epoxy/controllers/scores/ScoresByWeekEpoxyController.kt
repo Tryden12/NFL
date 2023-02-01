716e1398eb4721bf93155207c266011596a48b05
Tyler Ryden <tryden11@gmail.com>
2023-02-01 11:28:02 -0500

    all scores load on click based on week (final and upcoming) successfully

--- app/src/main/java/com/tryden/simplenfl/epoxy/controllers/scores/ScoresByWeekEpoxyController.kt
+++ app/src/main/java/com/tryden/simplenfl/epoxy/controllers/scores/ScoresByWeekEpoxyController.kt
@@ -13,6 +13,7 @@ import com.tryden.simplenfl.databinding.ModelScoresFinalWithHeaderItemBinding
 import com.tryden.simplenfl.databinding.ModelScoresScheduledWithHeaderItemBinding
 import com.tryden.simplenfl.epoxy.controllers.LoadingEpoxyModel
 import com.tryden.simplenfl.epoxy.controllers.models.*
+import com.tryden.simplenfl.network.response.teams.models.scores.Event
 import com.tryden.simplenfl.network.response.teams.models.scores.ScoreboardResponse
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
@@ -20,6 +21,8 @@ import java.time.format.DateTimeFormatter
 class ScoresByWeekEpoxyController(
     private val onWeekSelected: (String) -> Unit
 ): EpoxyController() {
+
+    private val TAG = ScoresByWeekEpoxyController::class.java.simpleName
 
     var isLoading: Boolean = true
         set(value){
@@ -60,32 +63,43 @@ class ScoresByWeekEpoxyController(
             return
         }
 
-
         /**
-         * Scores status: Scheduled
+         * Group scores by day
          **/
+        
         var headerTopFilled = false
-        var scheduledCount = 0
-//        for (i in scoresByWeekResponse!!.events.size-1 downTo 0) { // reverse
-        for (i in scoresByWeekResponse!!.events.indices) {
+        var eventCount = 0
+        var events = scoresByWeekResponse!!.events
+        if (events.isNotEmpty()) {
+            for (i in events.indices) {
 
-            when (scoresByWeekResponse!!.events[i].competitions[0].status.type.completed) {
-                false -> {
-                    if (scoresByWeekResponse!!.events[i].competitions[0].notes[0].headline.contains("pro bowl", ignoreCase = true)
-                    ) {
+                // Use header
+                if (eventCount == 0) {
+                    SectionHeaderCenteredEpoxyModel(
+                        sectionHeader = convertIsoDateToHeaderTitle(scoresByWeekResponse!!.events[i].date)
+                    ).id("header-$i").addTo(this)
+                    headerTopFilled = true
+                }
 
-//                        val date = scoresByWeekResponse!!.events[i].date
-//                        val headerDate = getWeekRange(date)
+                if (events[i].competitions[0].status.type.completed) {
+                    // Add scores final item
+                    ScoresFinalEpoxyModel(
+                        logoAway = events[i].competitions[0].competitors[1].team.logo.toString(),
+                        logoHome = events[i].competitions[0].competitors[0].team.logo.toString(),
+                        teamNameAway =events[i].competitions[0].competitors[1].team.shortDisplayName,
+                        teamNameHome = events[i].competitions[0].competitors[0].team.shortDisplayName,
+                        pointsAway = events[i].competitions[0].competitors[1].score,
+                        pointsHome = events[i].competitions[0].competitors[0].score,
+                        datePlayed = events[i].date,
+                        statusDesc = events[i].competitions[0].status.type.description,
+                        headline = ""
+                    ).id(events[i].id).addTo(this)
+                    eventCount++
+                } else {
+                    if (events[i].competitions[0].notes.isNotEmpty()
+                        && events[i].competitions[0].notes[0].headline.contains("pro bowl", ignoreCase = true) ) {
 
-                        // Use header
-                        if (scheduledCount == 0) {
-                            SectionHeaderCenteredEpoxyModel(
-                                sectionHeader = convertIsoDateToHeaderTitle(scoresByWeekResponse!!.events[i].date)
-                            ).id("header-playoffs-$i").addTo(this)
-                            headerTopFilled = true
-                        }
-
-                        // Add scores upcoming item
+                        // Add pro bowl
                         ScoresScheduledEpoxyModel(
                             logoAway = scoresByWeekResponse!!.events[i].competitions[0].competitors[1].team.logo.toString(),
                             logoHome = scoresByWeekResponse!!.events[i].competitions[0].competitors[0].team.logo.toString(),
@@ -96,20 +110,8 @@ class ScoresByWeekEpoxyController(
                             dateScheduled = scoresByWeekResponse!!.events[i].date,
                             broadcast = scoresByWeekResponse!!.events[i].competitions[0].geoBroadcasts[0].media.shortName,
                             headline = scoresByWeekResponse!!.events[i].competitions[0].notes[0].headline
-                        ).id(scoresByWeekResponse!!.events[i].id).addTo(this)
-                        scheduledCount++
-
+                        ).id(events[i].id).addTo(this)
                     } else {
-
-                        // Use header
-                        if (scheduledCount == 0) {
-                            SectionHeaderCenteredEpoxyModel(
-                                sectionHeader = convertIsoDateToHeaderTitle(scoresByWeekResponse!!.events[i].date)
-                            ).id("header-playoffs-$i").addTo(this)
-                            headerTopFilled = true
-                        }
-
-                        // Add scores upcoming item
                         ScoresScheduledEpoxyModel(
                             logoAway = scoresByWeekResponse!!.events[i].competitions[0].competitors[1].team.logo.toString(),
                             logoHome = scoresByWeekResponse!!.events[i].competitions[0].competitors[0].team.logo.toString(),
@@ -120,13 +122,10 @@ class ScoresByWeekEpoxyController(
                             dateScheduled = scoresByWeekResponse!!.events[i].date,
                             broadcast = scoresByWeekResponse!!.events[i].competitions[0].geoBroadcasts[0].media.shortName,
                             headline = scoresByWeekResponse!!.events[i].competitions[0].notes[0].headline
-                        ).id(scoresByWeekResponse!!.events[i].id).addTo(this)
-                        scheduledCount++
+                        ).id(events[i].id).addTo(this)
                     }
                 }
-                else -> {
-                    // nothing to do
-                }
+
             }
         }
 
@@ -134,27 +133,105 @@ class ScoresByWeekEpoxyController(
         if (headerTopFilled) {
             SectionBottomEpoxyModel(
                 useSection = true
-            ).id("bottom-home-upcoming-1").addTo(this)
+            ).id("bottom-1").addTo(this)
         }
+
 
 
         /**
-         * Scores status: final
+         * Scores status: Scheduled
          **/
-        headerTopFilled = false
-        scheduledCount = 0
-//        for (i in scoresByWeekResponse!!.events.size-1 downTo 0) { // reverse
-        for (i in scoresByWeekResponse!!.events.indices) {
-
-
-        }
-
-        // Add bottom to section
-        if (headerTopFilled) {
-            SectionBottomEpoxyModel(
-                useSection = true
-            ).id("bottom-home-upcoming-1").addTo(this)
-        }
+//        var headerTopFilled = false
+//        var scheduledCount = 0
+////        for (i in scoresByWeekResponse!!.events.size-1 downTo 0) { // reverse
+//        for (i in scoresByWeekResponse!!.events.indices) {
+//
+//            when (scoresByWeekResponse!!.events[i].competitions[0].status.type.completed) {
+//                false -> {
+//                    if (scoresByWeekResponse!!.events[i].competitions[0].notes[0].headline.contains("pro bowl", ignoreCase = true)
+//                    ) {
+//
+////                        val date = scoresByWeekResponse!!.events[i].date
+////                        val headerDate = getWeekRange(date)
+//
+//                        // Use header
+//                        if (scheduledCount == 0) {
+//                            SectionHeaderCenteredEpoxyModel(
+//                                sectionHeader = convertIsoDateToHeaderTitle(scoresByWeekResponse!!.events[i].date)
+//                            ).id("header-playoffs-$i").addTo(this)
+//                            headerTopFilled = true
+//                        }
+//
+//                        // Add scores upcoming item
+//                        ScoresScheduledEpoxyModel(
+//                            logoAway = scoresByWeekResponse!!.events[i].competitions[0].competitors[1].team.logo.toString(),
+//                            logoHome = scoresByWeekResponse!!.events[i].competitions[0].competitors[0].team.logo.toString(),
+//                            teamNameAway = scoresByWeekResponse!!.events[i].competitions[0].competitors[1].team.shortDisplayName,
+//                            teamNameHome = scoresByWeekResponse!!.events[i].competitions[0].competitors[0].team.shortDisplayName,
+//                            recordAway = "",
+//                            recordHome = "",
+//                            dateScheduled = scoresByWeekResponse!!.events[i].date,
+//                            broadcast = scoresByWeekResponse!!.events[i].competitions[0].geoBroadcasts[0].media.shortName,
+//                            headline = scoresByWeekResponse!!.events[i].competitions[0].notes[0].headline
+//                        ).id(scoresByWeekResponse!!.events[i].id).addTo(this)
+//                        scheduledCount++
+//
+//                    } else {
+//
+//                        // Use header
+//                        if (scheduledCount == 0) {
+//                            SectionHeaderCenteredEpoxyModel(
+//                                sectionHeader = convertIsoDateToHeaderTitle(scoresByWeekResponse!!.events[i].date)
+//                            ).id("header-playoffs-$i").addTo(this)
+//                            headerTopFilled = true
+//                        }
+//
+//                        // Add scores upcoming item
+//                        ScoresScheduledEpoxyModel(
+//                            logoAway = scoresByWeekResponse!!.events[i].competitions[0].competitors[1].team.logo.toString(),
+//                            logoHome = scoresByWeekResponse!!.events[i].competitions[0].competitors[0].team.logo.toString(),
+//                            teamNameAway = scoresByWeekResponse!!.events[i].competitions[0].competitors[1].team.shortDisplayName,
+//                            teamNameHome = scoresByWeekResponse!!.events[i].competitions[0].competitors[0].team.shortDisplayName,
+//                            recordAway = scoresByWeekResponse!!.events[i].competitions[0].competitors[1].records?.get(0)?.summary.toString(),
+//                            recordHome = scoresByWeekResponse!!.events[i].competitions[0].competitors[0].records?.get(0)?.summary.toString(),
+//                            dateScheduled = scoresByWeekResponse!!.events[i].date,
+//                            broadcast = scoresByWeekResponse!!.events[i].competitions[0].geoBroadcasts[0].media.shortName,
+//                            headline = scoresByWeekResponse!!.events[i].competitions[0].notes[0].headline
+//                        ).id(scoresByWeekResponse!!.events[i].id).addTo(this)
+//                        scheduledCount++
+//                    }
+//                }
+//                else -> {
+//                    // nothing to do
+//                }
+//            }
+//        }
+//
+//        // Add bottom to section
+//        if (headerTopFilled) {
+//            SectionBottomEpoxyModel(
+//                useSection = true
+//            ).id("bottom-home-upcoming-1").addTo(this)
+//        }
+//
+//
+//        /**
+//         * Scores status: final
+//         **/
+//        headerTopFilled = false
+//        scheduledCount = 0
+////        for (i in scoresByWeekResponse!!.events.size-1 downTo 0) { // reverse
+//        for (i in scoresByWeekResponse!!.events.indices) {
+//
+//
+//        }
+//
+//        // Add bottom to section
+//        if (headerTopFilled) {
+//            SectionBottomEpoxyModel(
+//                useSection = true
+//            ).id("bottom-home-upcoming-1").addTo(this)
+//        }
         
     }
 
