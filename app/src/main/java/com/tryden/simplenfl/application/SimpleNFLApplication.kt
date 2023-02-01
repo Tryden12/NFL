1f58a374ab650056ba28448adec17b7519037add
Tyler Ryden <tryden11@gmail.com>
2023-02-01 12:40:01 -0500

    zoneId available from application class

--- app/src/main/java/com/tryden/simplenfl/application/SimpleNFLApplication.kt
+++ app/src/main/java/com/tryden/simplenfl/application/SimpleNFLApplication.kt
@@ -2,15 +2,18 @@ package com.tryden.simplenfl.application
 
 import android.app.Application
 import android.content.Context
+import java.util.*
 
 class SimpleNFLApplication: Application() {
 
     companion object {
         lateinit var context: Context
+        lateinit var zoneId: String
     }
 
     override fun onCreate() {
         super.onCreate()
         context = this
+        zoneId = TimeZone.getDefault().id
     }
 }
